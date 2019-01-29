aef2947458fd3714aab1da03b4168e0957fd775f
claudiamalasquezaq <claudiamalasquez67@gmail.com>
2019-01-29 10:53:34 -0500

    Firebase project configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyA2z-mqmMBhZ5CMakXhAQ0aub9rexBUqxw",
+    authDomain: "projecttest-437e3.firebaseapp.com",
+    databaseURL: "https://projecttest-437e3.firebaseio.com",
+    projectId: "projecttest-437e3",
+    storageBucket: "projecttest-437e3.appspot.com",
+    messagingSenderId: "293468254995"
+};
